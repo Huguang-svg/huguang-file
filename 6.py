ef18cc28aa8df5d074b79c824332ae50bacca87b
Huguang-svg <3241305554@caa.edu.cn>
2025-11-27 09:56:09 +0800

    Add files via upload

--- 6.py
+++ 6.py
@@ -369,7 +369,7 @@ st.set_page_config(
 if "conversation_history" not in st.session_state:
     st.session_state.conversation_history = []
 if "selected_role" not in st.session_state:
-    st.session_state.selected_role = "人质"
+    st.session_state.selected_role = "Oct.yi"
 if "initialized" not in st.session_state:
     st.session_state.initialized = False
 
